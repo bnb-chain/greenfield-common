4aad1fe17906a270a7442c76ba6499ac62d820aa
DylanYong <dylan.y@nodereal.io>
2023-02-23 16:15:03 +0800

    feat: add challenge piece func

--- hash/checksum.go
+++ hash/checksum.go
@@ -21,16 +21,30 @@ func GenerateIntegrityHash(checksumList [][]byte) []byte {
 	return hash.Sum(nil)
 }
 
-// VerifyIntegrityHash verify integrity hash if right
-func VerifyIntegrityHash(integrityHash []byte, checksumList [][]byte, index int, pieceData []byte) error {
+// ChallengePieceHash challenge integrity hash and checksum list
+// integrityHash represents the integrity hash of one piece list, this piece list may be ec piece data list or
+// segment piece data list; if piece data list is ec, this list is all ec1 piece data; if piece list is segment, all
+// piece data is all segments of an object
+// checksumList is composed of  one piece list's individual checksum
+// index represents which piece is used to challenge
+// pieceData represents piece physical data that user want to challenge
+func ChallengePieceHash(integrityHash []byte, checksumList [][]byte, index int, pieceData []byte) error {
 	if len(checksumList) <= index {
-		return fmt.Errorf("checksum list is not correct")
+		return fmt.Errorf("invalid checksum list")
 	}
 	if !bytes.Equal(checksumList[index], GenerateChecksum(pieceData)) {
-		return fmt.Errorf("piece data checksum is not correct")
+		return fmt.Errorf("piece data and piece hash are inconsistent")
 	}
+	if err := VerifyIntegrityHash(integrityHash, checksumList); err != nil {
+		return err
+	}
+	return nil
+}
+
+// VerifyIntegrityHash verify integrity hash if right
+func VerifyIntegrityHash(integrityHash []byte, checksumList [][]byte) error {
 	if !bytes.Equal(integrityHash, GenerateIntegrityHash(checksumList)) {
-		return fmt.Errorf("piece data integrity checksum is not correct")
+		return fmt.Errorf("invalid integrity hash")
 	}
 	return nil
 }
